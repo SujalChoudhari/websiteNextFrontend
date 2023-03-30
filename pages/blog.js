dc0b6ec0469b87f1310fbaf94ede252d6ab4e0fb
Sujal Choudhari <sjlchoudhari@gmail.com>
2023-03-30 14:13:29 +0530

    Issue in Blog Fixed
    Issue was caused because there was not tags in the post

--- pages/blog.js
+++ pages/blog.js
@@ -8,42 +8,41 @@ function Blog(props) {
     const { blogs } = props;
     return (
         <>
-            <section className="text-gray-400 bg-black body-font overflow-hidden">
-                <section className="text-gray-400 bg-black body-font">
-                    <div className="container px-5 py-14 mx-auto">
-                        <div className="lg:w-2/3 flex flex-col sm:flex-row sm:items-center items-start mx-auto">
-                            <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-white">I have collected a lot of information in the journey. I am sharing some of them here.</h1>
-                        </div>
+            <section className="bg-black text-gray-100">
+                <div className="container px-5 py-14 mx-auto">
+                    <div className="text-center">
+                        <h1 className="text-3xl font-bold leading-tight mb-4">
+                            I have collected a lot of information in the journey. I am sharing some of them here.
+                        </h1>
                     </div>
-                </section>
+                </div>
                 <div className="container px-5 py-24 mx-auto">
-                    <div className="-my-8 divide-y-2 divide-gray-800">
-
-                        {
-                            blogs.map((blog) => {
-                                return (
-                                    <div key={blog._id} className="py-8 flex flex-wrap md:flex-nowrap">
-                                        <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                                            <span className="font-semibold title-font text-white">{blog.tags[0].title}</span>
-                                            <span className="mt-1 text-gray-500 text-sm">{blog.date}</span>
-                                        </div>
-                                        <div className="md:flex-grow">
-                                            <h2 className="text-2xl font-medium text-white title-font">{blog.title}</h2>
-                                            <Link href={`blogpost/${blog._id}`} className="text-blue-400 inline-flex items-center mt-1">Read
-                                                <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                                    <path d="M5 12h14"></path>
-                                                    <path d="M12 5l7 7-7 7"></path>
-                                                </svg>
-                                            </Link>
-                                        </div>
-                                    </div>)
-                            })
-                        }
-
-
+                    <div className="max-w-3xl mx-auto">
+                        {blogs.map((blog) => {
+                            return (
+                                <div key={blog._id} className="py-8 flex flex-col sm:flex-row items-start">
+                                    <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                                        <span className="font-semibold text-white">{blog.tags?.at(0).title}</span>
+                                        <span className="mt-1 text-gray-500 text-sm">{blog.date}</span>
+                                    </div>
+                                    <div className="md:flex-grow">
+                                        <h2 className="text-2xl font-medium text-white leading-tight mb-2">{blog.title}</h2>
+                                        <Link href={`blogpost/${blog._id}`} className="inline-flex items-center text-blue-400 hover:text-blue-500">
+                                            Read more
+                                            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                                <path d="M5 12h14"></path>
+                                                <path d="M12 5l7 7-7 7"></path>
+                                            </svg>
+                                        </Link>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </section>
+
+
         </>
     )
 }
